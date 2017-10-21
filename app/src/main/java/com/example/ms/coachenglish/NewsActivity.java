12dc6c0399c0f49121a1fdbe6dc1fdd0393a2f96
MS2019 <1135573080@qq.com>
2017-10-21 20:56:49 +0800

    commit

--- app/src/main/java/com/example/ms/coachenglish/NewsActivity.java
+++ app/src/main/java/com/example/ms/coachenglish/NewsActivity.java
@@ -2,9 +2,11 @@ package com.example.ms.coachenglish;
 
 import android.content.ClipboardManager;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,16 +14,26 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.lang.reflect.Field;
 
 public class NewsActivity extends AppCompatActivity {
 
     private WebView webView;
     private AlertDialog.Builder alertDialog;
-    private TextView getDialog_word_textview;
+    private TextView dialog_word_textview;
     private TextView dialog_phometic_textview;
     private TextView dialog_tran_textview;
+    private CharSequence addedText;
+
+    private String word;
+    private String phonetic;
+    private String tran;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +52,13 @@ public class NewsActivity extends AppCompatActivity {
         //扩大比例的缩放
         webView.getSettings().setUseWideViewPort(true);
         //自适应屏幕
-        // webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        //webView.getSettings().setUseWideViewPort(true);
         //webView.getSettings().setLoadWithOverviewMode(true);
 
         registerClipEvents();
 
     }
+
 
     //监听剪切板获取剪切板内容
     private void registerClipEvents() {
@@ -54,9 +67,12 @@ public class NewsActivity extends AppCompatActivity {
             @Override
             public void onPrimaryClipChanged() {
                 if (manager.hasPrimaryClip() && manager.getPrimaryClip().getItemCount() > 0) {
-                    CharSequence addedText = manager.getPrimaryClip().getItemAt(0).getText();
+                    addedText = manager.getPrimaryClip().getItemAt(0).getText();
                     if (addedText != null) {
                         dialog(addedText);
+                    }
+                    else{
+                        Toast.makeText(MyApplication.getContext(),"没有选中任何内容",Toast.LENGTH_SHORT).show();
                     }
                 }
             }
@@ -95,19 +111,37 @@ public class NewsActivity extends AppCompatActivity {
         alertDialog.setView(view);
         //alertDialog.setTitle(addedText);
 
-        getDialog_word_textview = (TextView)view.findViewById(R.id.dialog_word_textview);
+        dialog_word_textview = (TextView)view.findViewById(R.id.dialog_word_textview);
         dialog_phometic_textview = (TextView)view.findViewById(R.id.dialog_phometic_textview);
         dialog_tran_textview = (TextView)view.findViewById(R.id.dialog_tran_textview);
 
-        getDialog_word_textview.setText(addedText);
-        dialog_phometic_textview.setText("你好");
-        dialog_tran_textview.setText("年后");
+        try{
+            WorkTask workTask = new WorkTask();
+            workTask.execute();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         alertDialog.setPositiveButton("收藏", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
+                if(TextUtils.isEmpty(phonetic) && TextUtils.isEmpty(tran)){
+                    Toast.makeText(MyApplication.getContext(), "请翻译之后再收藏", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Words words = new Words();
+                    words.setWord(word);
+                    words.setPhonetic(phonetic);
+                    words.setTran(tran);
+                    words.save();
+                    if(words.save()){
+                        Toast.makeText(MyApplication.getContext(),"收藏成功",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(MyApplication.getContext(),"收藏失败",Toast.LENGTH_SHORT).show();
+                    }
 
-
-
+                }
 
             }
         });
@@ -116,12 +150,47 @@ public class NewsActivity extends AppCompatActivity {
         alertDialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
+
             }
         });
         alertDialog.show();
     }
 
 
+
+    //对网络进行操作的内部类
+    class WorkTask extends AsyncTask {                             //AsyncTask是Android框架中的一个轻量线程池
+        String url = "http://dict.youdao.com/search?q=";
+
+        public WorkTask() {
+            word = addedText.toString();
+            url = url + word;
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {             //将在onPreExecute 方法执行后马上执行，该方法运行在后台线程中。这里执行从网站获取音标和解释。
+            try {
+                Document document = Jsoup.connect(url).timeout(3000).get();
+                tran = document.select(".trans-container").first().text();
+                phonetic = document.select(".phonetic").first().text();
+
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {           //在doInBackground 执行完成后，onPostExecute 方法将被UI thread调用，后台的计算结果将通过该方法传递到UI thread。
+            super.onPostExecute(o);
+            if(!TextUtils.isEmpty(word)){
+                dialog_word_textview.setText(word);
+                dialog_phometic_textview.setText(phonetic);
+                dialog_tran_textview.setText(tran);
+            }
+        }
+    }
 
 
 }
